51c8bf53a219b363178369245c3e081c4644b232
edgarbasurto <edgar.basurtoc@ug.edu.ec>
2023-03-12 09:47:00 -0500

    Add Aprendizaje
    Add: Modulo Aprendizaje

---
+++ lib/src/data/data.dart
@@ -0,0 +1,146 @@
+import 'package:proyecto_dam_p2/src/models/TileModel.dart';
+
+String selectedTile = "";
+int selectedIndex = -1;
+bool selected = true;
+int points = 0;
+
+List<TileModel> myPairs = <TileModel>[];
+List<bool> clicked = <bool>[];
+
+List<bool> getClicked() {
+  List<bool> yoClicked = <bool>[];
+  List<TileModel> myairs = <TileModel>[];
+  myairs = getPairs();
+  for (int i = 0; i < myairs.length; i++) {
+    yoClicked[i] = false;
+  }
+
+  return yoClicked;
+}
+
+List<TileModel> getPairs() {
+  List<TileModel> pairs = <TileModel>[];
+
+  TileModel tileModel = TileModel();
+
+  //1
+  tileModel.setImageAssetPath("assets/img/ic_m01.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //2
+  tileModel.setImageAssetPath("assets/img/ic_m02.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //3
+  tileModel.setImageAssetPath("assets/img/ic_m03.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //4
+  tileModel.setImageAssetPath("assets/img/ic_m04.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+  //5
+  tileModel.setImageAssetPath("assets/img/ic_m05.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //6
+  tileModel.setImageAssetPath("assets/img/ic_m06.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //7
+  tileModel.setImageAssetPath("assets/img/ic_m07.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //8
+  tileModel.setImageAssetPath("assets/img/ic_m08.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  return pairs;
+}
+
+List<TileModel> getQuestionPairs() {
+  List<TileModel> pairs = <TileModel>[];
+
+  TileModel tileModel = TileModel();
+
+  //1
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //2
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //3
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //4
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+  //5
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //6
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //7
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  //8
+  tileModel.setImageAssetPath("assets/question.png");
+  tileModel.setIsSelected(false);
+  pairs.add(tileModel);
+  pairs.add(tileModel);
+  tileModel = TileModel();
+
+  return pairs;
+}
--- lib/src/pages/aprendizaje_page.dart
+++ lib/src/pages/aprendizaje_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:proyecto_dam_p2/src/data/data.dart';
+import 'package:proyecto_dam_p2/src/models/TileModel.dart';
 import 'package:proyecto_dam_p2/src/pages/helpers_widget.dart';
 
 class AprendizajePage extends StatelessWidget {
@@ -8,14 +12,312 @@ class AprendizajePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarra(context, "", true),
-      body: Center(
-          child: Column(children: <Widget>[
-        Text(
-          'Contenido',
-          style: TextStyle(fontSize: 24),
+      body: Memorama(),
+    );
+  }
+}
+
+class Memorama extends StatefulWidget {
+  @override
+  _MemoramaState createState() => _MemoramaState();
+}
+
+class _MemoramaState extends State<Memorama> {
+  List<TileModel> gridViewTiles = [];
+  List<TileModel> questionPairs = [];
+  int seconds = 0, minutes = 0, hours = 0;
+  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
+  Timer? timer;
+  bool started = false;
+  List laps = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    reStart();
+    if (!started) {
+      start();
+    }
+  }
+
+  void stop() {
+    timer!.cancel();
+    setState(() {
+      started = false;
+    });
+  }
+
+  void reset() {
+    timer!.cancel();
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+
+      digitSeconds = "00";
+      digitMinutes = "00";
+      digitHours = "00";
+
+      started = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digitHours:$digitMinutes:$digitSeconds";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  void start() {
+    started = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+
+      if (localSeconds > 59) {
+        if (localMinutes > 59) {
+          localHours++;
+          localMinutes = 0;
+        } else {
+          localMinutes++;
+          localSeconds = 0;
+        }
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
+        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
+        digitHours = (hours >= 10) ? "$hours" : "0$hours";
+      });
+    });
+  }
+
+  void reStart() {
+    myPairs = getPairs();
+    myPairs.shuffle();
+
+    gridViewTiles = myPairs;
+    Future.delayed(const Duration(seconds: 5), () {
+// Here you can write your code
+      setState(() {
+        print("2 seconds done");
+        // Here you can write your code for open new view
+        questionPairs = getQuestionPairs();
+        gridViewTiles = questionPairs;
+        selected = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 40,
+              ),
+              points != 800
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "$points/800",
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        const Text(
+                          "Puntuación",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    )
+                  : Container(),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Text("$digitHours:$digitMinutes:$digitSeconds",
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ),
+              points != 800
+                  ? GridView(
+                      shrinkWrap: true,
+                      //physics: ClampingScrollPhysics(),
+                      scrollDirection: Axis.vertical,
+                      gridDelegate:
+                          const SliverGridDelegateWithMaxCrossAxisExtent(
+                              mainAxisSpacing: 0.0, maxCrossAxisExtent: 100.0),
+                      children: List.generate(gridViewTiles.length, (index) {
+                        return Tile(
+                          imagePathUrl:
+                              gridViewTiles[index].getImageAssetPath(),
+                          tileIndex: index,
+                          parent: this,
+                        );
+                      }),
+                    )
+                  : Container(
+                      child: Column(
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              points = 0;
+                              reStart();
+                            });
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 200,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: const Text(
+                              "Reintentar",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // TODO
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 200,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue, width: 2),
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: const Text(
+                              "Rate Us",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))
+            ],
+          ),
         ),
-        SalirBoton(context)
-      ])),
+      ),
+    );
+  }
+}
+
+class Tile extends StatefulWidget {
+  Tile(
+      {required this.imagePathUrl,
+      required this.tileIndex,
+      required this.parent});
+
+  String imagePathUrl;
+  int tileIndex;
+  _MemoramaState parent;
+
+  @override
+  _TileState createState() => _TileState();
+}
+
+class _TileState extends State<Tile> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (!selected) {
+          setState(() {
+            myPairs[widget.tileIndex].setIsSelected(true);
+          });
+          if (selectedTile != "") {
+            /// testing if the selected tiles are same
+            if (selectedTile == myPairs[widget.tileIndex].getImageAssetPath()) {
+              print("add point");
+              points = points + 100;
+              print(selectedTile + " thishis" + widget.imagePathUrl);
+
+              TileModel tileModel = TileModel();
+              print(widget.tileIndex);
+              selected = true;
+              Future.delayed(const Duration(seconds: 2), () {
+                tileModel.setImageAssetPath("");
+                myPairs[widget.tileIndex] = tileModel;
+                print(selectedIndex);
+                myPairs[selectedIndex] = tileModel;
+                this.widget.parent.setState(() {});
+                setState(() {
+                  selected = false;
+                });
+                selectedTile = "";
+              });
+            } else {
+              print(selectedTile +
+                  " thishis " +
+                  myPairs[widget.tileIndex].getImageAssetPath());
+              print("wrong choice");
+              print(widget.tileIndex);
+              print(selectedIndex);
+              selected = true;
+              Future.delayed(const Duration(seconds: 2), () {
+                this.widget.parent.setState(() {
+                  myPairs[widget.tileIndex].setIsSelected(false);
+                  myPairs[selectedIndex].setIsSelected(false);
+                });
+                setState(() {
+                  selected = false;
+                });
+              });
+
+              selectedTile = "";
+            }
+          } else {
+            setState(() {
+              selectedTile = myPairs[widget.tileIndex].getImageAssetPath();
+              selectedIndex = widget.tileIndex;
+            });
+
+            print(selectedTile);
+            print(selectedIndex);
+          }
+        }
+      },
+      child: Container(
+        margin: const EdgeInsets.all(5),
+        child: myPairs[widget.tileIndex].getImageAssetPath() != ""
+            ? Image.asset(myPairs[widget.tileIndex].getIsSelected()
+                ? myPairs[widget.tileIndex].getImageAssetPath()
+                : widget.imagePathUrl)
+            : Container(
+                color: Colors.white,
+                child: Image.asset("assets/correct.png"),
+              ),
+      ),
     );
   }
 }
