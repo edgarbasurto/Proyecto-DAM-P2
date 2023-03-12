ccb33e3e883f30b1bb176b96f80b4009652c04a6
Iván Espinoza de los Monteros <ivanespinoza2499@gmail.com>
2023-03-11 20:51:58 -0500

    Diseño modulo atencion

--- lib/src/pages/atencion_page.dart
+++ lib/src/pages/atencion_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:proyecto_dam_p2/src/pages/helpers_widget.dart';
 
 class AtencionPage extends StatelessWidget {
@@ -7,15 +10,197 @@ class AtencionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarra(context, "", true),
-      body: Center(
-          child: Column(children: <Widget>[
-        Text(
-          'Contenido',
-          style: TextStyle(fontSize: 24),
+        appBar: AppBarra(context, "", true),
+        body: Documento(context) //documento principal del diseño
+        );
+  }
+
+  Column Documento(BuildContext context) {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[Cuerpo(), Chronometer(), Score(), SalirBoton(context)],
+    );
+  }
+
+  Column Cuerpo() {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        ImagenTop(),
+        Indicaciones(),
+        Respuestas(),
+      ],
+    );
+  }
+
+  Container ImagenTop() {
+    return new Container(
+      height: 130,
+      child: Image.asset("assets/img/ic_uglogo.png", width: 120, height: 120),
+    );
+  }
+
+  Text Indicaciones() {
+    return new Text(
+      "Aquí va ir la pregunta",
+      style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 22,
+          fontFamily: "sans-serif-medium"),
+    );
+  }
+
+  Text Score() {
+    return new Text(
+      "Aquí va ir el score",
+      style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 22,
+          fontFamily: "sans-serif-medium"),
+    );
+  }
+
+  Text Chronometer() {
+    return new Text(
+      "Aquí va ir el chronometer",
+      style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 22,
+          fontFamily: "sans-serif-medium"),
+    );
+  }
+
+  Column Respuestas() {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Linea1(),
+        Linea2(),
+        Linea3(),
+        Linea4(),
+      ],
+    );
+  }
+
+  Row Linea1() {
+    return new Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
         ),
-        SalirBoton(context)
-      ])),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        )
+      ],
+    );
+  }
+
+  Row Linea2() {
+    return new Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        )
+      ],
+    );
+  }
+
+  Row Linea3() {
+    return new Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        )
+      ],
+    );
+  }
+
+  Row Linea4() {
+    return new Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        ),
+        Container(
+          height: 60,
+          child: Image.asset("assets/img/ic_uglogo.png", width: 70, height: 60),
+        )
+      ],
     );
   }
 }
